95aa6b58afa764ba89541a407ed39fce41ab65e2
jmorc <jeffcaves@gmail.com>
2015-03-25 20:24:04 -0700

    Nested questions collection in quiz controller

--- app/assets/javascripts/models/quiz.js
+++ app/assets/javascripts/models/quiz.js
@@ -1,3 +1,17 @@
 Rehearsable.Models.Quiz = Backbone.Model.extend({
-	urlRoot: '/api/quizzes'
-})
+	urlRoot: '/api/quizzes',
+
+	questions: function () {
+		this._questions = this._questions ||
+		  new Rehearsable.Collections.Questions([], { quiz: this });
+		return this._questions;
+	},
+
+	parse: function(payload) {
+		if (payload.questions) {
+		  this.questions().set(payload.question, { parse: true });
+		  delete payload.questions
+		}
+		return payload;
+	}
+});
